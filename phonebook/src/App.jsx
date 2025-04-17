baef081b10042558af4e5ead1479f1a9c4fbd840
KamiKruse <altsake@protonmail.com>
2025-04-17 20:06:43 +0530

    Finished exercise 2.7

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,22 +1,47 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "hi" }]);
   const [newName, setNewName] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    let test = persons.filter((person) => {
+      return person.name === newName;
+    });
+    if (test.length > 0) {
+      alert(`${newName} is already added in the phonebook`);
+      setNewName("");
+    } else {
+      setPersons((prevPersons) => [...prevPersons, { name: newName }]);
+      setNewName("");
+    }
+  };
+  
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={handleClick} type="submit">
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+
+      {persons.map((person) => (
+        <li key={person.name} style={{ listStyle: "none" }}>
+          {person.name}
+        </li>
+      ))}
     </div>
   );
 };
