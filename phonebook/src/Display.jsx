a7d67af550de5a4f502ef8859b6087ce1a6ce3c2
KamiKruse <altsake@protonmail.com>
2025-04-18 12:23:19 +0530

    Completed exercise 2.11 and added db.json serving data to app.jsx

--- phonebook/src/Display.jsx
+++ phonebook/src/Display.jsx
@@ -3,9 +3,12 @@ const Display = (props) => {
   return props.persons.map(
     (person) =>
       person.name.toLowerCase().includes(props.filter.toLowerCase()) && (
-        <>
-          <List id={person.id} key={person.id} name={person.name} number={person.number} />
-        </>
+        <List
+          id={person.id}
+          key={person.id}
+          name={person.name}
+          number={person.number}
+        />
       )
   );
 };
